b3b5616827937500f2988993769b7c3b0da4a28e
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-06-23 14:36:03 +0700

    create and styling Footer component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
 import FeaturedProperty from '../../components/featuredProperties/FeaturedProperty';
 import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
 
 const Home = () => {
     return (
@@ -18,6 +19,7 @@ const Home = () => {
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperty />
                 <MailList />
+                <Footer />
             </div>
         </>
     );
